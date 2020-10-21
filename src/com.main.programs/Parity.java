6256236ca3035234d6ba421cbc81543a53d3511d
PrajnyaPrabhu19 <prajnya.prabhu@email.ucr.edu>
2020-10-20 18:24:28 -0700

    added gitignore

--- src/com.main.programs/Parity.java
+++ src/com.main.programs/Parity.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class Parity{
 
+    //brute force O(n)
     public static short parity(long x){
         short result =0;
         while(x!=0){
@@ -17,9 +18,20 @@ public class Parity{
         return result;
     }
 
+    //x&(x-1) =x; O(k) where k  = number of 1 bits in x
+    public static short parity1(long x){
+        short result =0;
+        while(x!=0){
+            result ^=1;
+            x &= (x-1);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Random ran = new Random();
         long n = ran.nextLong();
         System.out.println("Parity of "+n+" is "+parity(n));
+        System.out.println("Parity of "+n+" is "+parity1(n));
     }
 }
