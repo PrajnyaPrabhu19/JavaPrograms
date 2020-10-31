38cc51345c2d8540aac78a5215af6f290a635475
PrajnyaPrabhu19 <prajnya.prabhu@email.ucr.edu>
2020-10-30 19:08:37 -0700

    Commonly used node classes

---
+++ src/com.main.programs/ListNode.java
@@ -0,0 +1,17 @@
+package com.main.programs;
+/*
+ListNode class with constructors
+ */
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
---
+++ src/com.main.programs/TreeNode.java
@@ -0,0 +1,21 @@
+package com.main.programs;
+/*
+TreeNode class with constructors
+ */
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
