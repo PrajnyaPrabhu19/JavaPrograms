bfbe6ff0faa84a1ee52711a340ab3f46967fbf51
PrajnyaPrabhu19 <prajnya.prabhu@email.ucr.edu>
2020-11-14 12:32:58 -0800

    reverse sublist

--- src/com.main.programs/ReverseSubList.java
+++ src/com.main.programs/ReverseSubList.java
@@ -9,7 +9,7 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
-public static class ReverseSubList{
+public class ReverseSubList{
     public static class ListNode {
         int val;
         ListNode next;
@@ -17,6 +17,30 @@ public static class ReverseSubList{
             val = x;
             next = null;
         }
+    }
+
+    public static ListNode reverseList(ListNode L, int start, int finish){
+        if(L==null){
+            return L;
+        }
+        if(start==finish){
+            return L;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = L;
+        int k =1;
+        ListNode subList = dummy;
+        while(k++<start){
+            subList=subList.next;
+        }
+        ListNode sublistIterator = subList.next;
+        while (start++<finish){
+            ListNode temp = sublistIterator.next;
+            sublistIterator.next = temp.next;
+            temp.next = subList.next;
+            subList.next = temp;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
@@ -27,8 +51,27 @@ public static class ReverseSubList{
         ListNode n4 = new ListNode(4);
         ListNode n5 = new ListNode(5);
         ListNode n6 = new ListNode(6);
+        n1.next =n2;
+        n2.next =n3;
+        n3.next =n4;
+        n4.next =n5;
+        n5.next =n6;
+        ListNode p =h1;
+        while(p!=null && p.next!=null){
+            System.out.print(p.val+"->");
+            p=p.next;
+        }
+        System.out.print(p.val);
 
-
+        System.out.println("\nReverse List");
+        h1 = reverseList(h1, 2,5);
+        p =h1;
+        while(p!=null && p.next!=null){
+            System.out.print(p.val+"->");
+            p=p.next;
+        }
+        System.out.print(p.val);
+        System.out.println("");
     }
 
 }
