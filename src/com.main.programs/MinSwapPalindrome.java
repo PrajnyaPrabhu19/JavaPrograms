03b455d7c59c9a7ecde2ff116fc76cc7f00d30bd
PrajnyaPrabhu19 <prajnya.prabhu@email.ucr.edu>
2020-11-24 08:53:41 -0800

    minimum swaps to form palindrome

---
+++ src/com.main.programs/MinSwapPalindrome.java
@@ -0,0 +1,88 @@
+package com.main.programs;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+/*
+Given a string, what is the minimum number of adjacent swaps required to convert a string into a palindrome.
+If not possible, return -1.
+Input: "mamad"
+Output: 3
+ */
+
+public class MinSwapPalindrome{
+
+    public static boolean canPalindrome(String s){
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                set.remove(s.charAt(i));
+            }
+            else{
+                set.add(s.charAt(i));
+            }
+        }
+        if(set.size()>1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static void swap(char[] chars, int k, int i) {
+        char temp = chars[k];
+        chars[k] = chars[i];
+        chars[i] = temp;
+    }
+
+
+    public static int minswap(String s){
+        if(s==null || s.length()==0){
+            return -1;
+        }
+        int l = s.length();
+        if(l==1) return 0;
+        boolean pal = canPalindrome(s);
+        if(pal){
+            int swaps = 0;
+            char[] arr = s.toCharArray();
+            int p=0, q= l-1;
+            while(p<q){
+                if(arr[p] == arr[q]){
+                    p++;
+                    q--;
+                }
+                else{
+                    //p and qth chars not same, find a char at k to be swapped with q
+                    int k =q;
+                    while(k>p && arr[k]!= arr[p]){
+                        k--;
+                    }
+                    if(k==p){
+                        swap(arr, p, p+1);
+                        swaps++;
+                    }
+                    else{
+                        while(k<q){
+                            swap(arr, k,k+1);
+                            k++;
+                            swaps++;
+                        }
+                        p++;q--;
+                    }
+                }
+            }
+            return swaps;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "mamad";
+        String s2 = "abcdf";
+        System.out.println("The minimum number of adjacent swaps required to convert a string '"+s1+"' to palindrome is: "+minswap(s1));
+        System.out.println("The minimum number of adjacent swaps required to convert a string '"+s2+"' to palindrome is: "+minswap(s2));
+    }
+}
